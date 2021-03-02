5183e466be99c8b872a9173855aa306e5eb305bd
Armandik <Armandik@users.noreply.github.com>
2021-03-02 19:12:24 +0100

    fixed insertion

--- main.cpp
+++ main.cpp
@@ -1,17 +1,38 @@
 #include "map.h"
 #include <stdio.h>
 #include <string>
+#include <map>
+#include <chrono>
 
 int main() {
 
-	map<std::string> culo;
-	
-	culo.insert(10, "a");
-	culo.insert(1, "b");
-	culo.insert(2, "c");
-	culo.insert(15, "d");
+	map<int> mappaMia;
+	std::map<int, int> mappaSTL;
 
-	culo.printTree(culo.root());
+	auto start = std::chrono::high_resolution_clock::now();
+	auto stop = std::chrono::high_resolution_clock::now();
 
-	return 6;
+	for (int i = 0; i < 50; i++) {
+		int x = rand();
+		mappaMia.insert(x, x);
+	}
+	stop = std::chrono::high_resolution_clock::now();
+
+	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+	std::cout << "mappa mia, tempo: " << float(duration.count()) / 1000 << " millisecondi, " << mappaMia.size() << " elementi\n";
+
+	start = std::chrono::high_resolution_clock::now();
+	for (int i = 0; i < 50; i++) {
+		long int x = rand();
+		mappaSTL.insert(std::pair<int, int>(x, x));
+	}
+	stop = std::chrono::high_resolution_clock::now();
+
+	duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+	std::cout << "mappa stl, tempo: " << float(duration.count()) / 1000 << " millisecondi, " << mappaSTL.size() << " elementi\n";;
+
+	mappaMia.printTree(mappaMia.root());
+	printf("\n");
+
+	return 0;
 }
--- map.h
+++ map.h
@@ -17,9 +17,9 @@ private:
 
 public:
 
-	node* parent;
 	node* left;
 	node* right;
+	int height;
 
 	node(int key, T value);
 	~node();
@@ -39,21 +39,27 @@ class map {
 private:
 
 	node<T>* _root;
+	int _size;
 
-	// void rightRotation(node<T>* head);
-	// void leftRotation(node<T>* head);
-	int height(node<T>* head);
+	node<T>* rotation_right(node<T>* head);
+	node<T>* rotation_left(node<T>* head);
+	node<T>* rotation_rightleft(node<T>* head);
+	node<T>* rotation_leftright(node<T>* head);
+	node<T>* recursiveInsert(node<T>* head, int key, T value);
 	int balanceFactor(node<T>* head);
-	node<T>* recursiveInsert(node<T>* head, node<T>* prnt, int key, T value);
 
 public:
 
 	map();
 	~map();
 
+	int size() { return _size; }
+	int height(node<T>* head);
 	node<T>* search(int key);
 	void insert(int key, T value);
 	void printTree(node<T>* head);
+	void erase(node<T>* head);
+	void erase(int key) { erase(search(key)); }
 
 	// Getters
 	node<T>* root() { return _root; }
@@ -69,9 +75,10 @@ inline node<T>::node(int key, T value) {
 	_values.first = key;
 	_values.second = value;
 
-	parent = 0;
 	left = 0;
 	right = 0;
+
+	height = 0;
 }
 
 template<class T>
@@ -90,6 +97,7 @@ template<class T>
 map<T>::map() {
 
 	_root = 0;
+	_size = 0;
 
 }
 
@@ -100,7 +108,7 @@ map<T>::~map() {
 
 }
 
-template<class T>
+template<class T>// Returns the node that contains the specified key
 node<T>* map<T>::search(int key) {
 
 	node<T>* v = _root;
@@ -118,58 +126,131 @@ node<T>* map<T>::search(int key) {
 }
 
 template<class T>
-node<T>* map<T>::recursiveInsert(node<T>* head, node<T>* prnt, int key, T value) {
+node<T>* map<T>::recursiveInsert(node<T>* head, int key, T value) {
 
+	// Create the new node if head is not a node
 	if (head == 0) {
 
+		_size++;
 		node<T>* tmp = new node<T>(key, value);
-		tmp->parent = prnt;
 		return tmp;
 	}
-	else {
 
-		if (key < head->key()) 
-			head->left = recursiveInsert(head->left, head, key, value);
-		else if (key > head->key())
-			head->right = recursiveInsert(head->right, head, key, value);
+	// Recursively search for the location where the new node will be placed
+	if (key < head->key()) 
+		head->left = recursiveInsert(head->left, key, value);
+	else if (key > head->key())
+		head->right = recursiveInsert(head->right, key, value);
 
-		return head;
+	// Update height
+	head->height = 1 + std::max(height(head->left), height(head->right));
+	
+	// Balance tree through rotations
+	int bal = balanceFactor(head);
+	if (bal > 1) { // unbalanced left subtree
+		if (key < head->left->key())
+			return rotation_right(head);
+		else
+			return rotation_leftright(head);
 	}
+	else if (bal < -1) { // unbalanced right subtree
+		if (key > head->right->key()) {
+			return rotation_left(head);
+		}
+		else
+			return rotation_rightleft(head);
+	}
+	
+	return head;
 
 }
 
-template<class T>
+template<class T>// Insertion of a node
 void map<T>::insert(int key, T value) {
 
-	_root = recursiveInsert(_root, 0, key, value);
+	_root = recursiveInsert(_root, key, value);
 }
 
-template<class T>
+template<class T>// In order print of the tree
 void map<T>::printTree(node<T>* head) {
 
 	if (head == 0)
 		return;
 
 	printTree(head->left);
-	std::cout << head->value() << ", height:" << height(head) << ", balance" << balanceFactor(head)<< "\n";
+	std::cout << head->value() << ", height:" << height(head) << ", balance " << balanceFactor(head)<< "\n";
 	printTree(head->right);
 }
 
-template<class T>
+template<class T>// Erase node
+void map<T>::erase(node<T>* head){
+
+
+
+}
+
+template<class T>// Right rotation for balancing
+node<T>* map<T>::rotation_right(node<T>* head){
+
+	node<T>* tmp = head->left;
+	head->left = tmp->right;
+	tmp->right = head;
+
+	// Height adjustments
+	head->height = 1 + std::max(height(head->left), height(head->right));
+	tmp->height = 1 + std::max(height(tmp->left), height(tmp->right));
+
+	return tmp;
+
+}
+
+template<class T>// Left rotation for balancing
+node<T>* map<T>::rotation_left(node<T>* head){
+
+	node<T>* tmp = head->right;
+	head->right = tmp->left;
+	tmp->left = head;
+
+	// Height adjustments
+	head->height = 1 + std::max(height(head->left), height(head->right));
+	tmp->height = 1 + std::max(height(tmp->left), height(tmp->right));
+
+	return tmp;
+
+}
+
+template<class T>// Right+Left rotation for balancing
+node<T>* map<T>::rotation_rightleft(node<T>* head){
+
+	node<T>* tmp = head->right;
+	head->right = rotation_right(tmp);
+	return rotation_left(head);
+
+}
+
+template<class T>// Left+Right rotation for balancing
+node<T>* map<T>::rotation_leftright(node<T>* head){
+
+	node<T>* tmp = head->left;
+	head->left = rotation_left(tmp);
+	return rotation_right(head);
+
+}
+
+template<class T>// Returns height of the node
 int map<T>::height(node<T>* head) {
 
 	if (head == 0)
 		return -1;
 	else
-		return (std::max(height(head->left), height(head->right)) + 1);
+		return head->height;
 }
 
-template<class T>
+template<class T>// Returns balance factor of the node
 int map<T>::balanceFactor(node<T>* head) {
 
 	if (head == 0)
 		return 0;
 	else
 		return(height(head->left) - height(head->right));
-
 }
